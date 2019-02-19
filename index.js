a9792fd3e9aaf46c4e83d7a4364824b6d5e9d9d5
Latanya Smalley <smalleylatanya@yahoo.com>
2019-02-19 08:11:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ console.error("ERRORO!")
 console.log("UPDATE!")
 console.warn("WARNING")
 console.error("ERROR!")
+//declare the variable
+var word
+word='bird'
+console.log(word)//'bird'
+word='dog'
+console.log(word)//'dog'
+
+
